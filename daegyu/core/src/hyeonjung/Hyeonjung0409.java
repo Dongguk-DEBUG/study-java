c4925b45fe4e34113a839b28fd395d8494f8120b
HyeonJung48 <hjoogkim@naver.com>
2025-04-09 19:10:24 +0900

    Create Hyeonjung0409.java

---
+++ daegyu/core/src/hyeonjung/Hyeonjung0409.java
@@ -0,0 +1,11 @@
+package hyeonjung;
+
+public class Hyeonjung0409 {
+    public static void main(String[]args){
+        int num = 3;
+        double pi = 3.14;
+        String hi = "hello";
+        char small = 'A';
+        boolean check = true;
+    }
+}
