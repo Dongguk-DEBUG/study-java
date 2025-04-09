3b16c4da9c0d1f8667901b952e2aef7ce1b469e5
daegyu-song <sdk0109@naver.com>
2025-04-09 19:02:04 +0900

    Create Daegyu0409.java

---
+++ daegyu/core/src/daegyu/Daegyu0409.java
@@ -0,0 +1,12 @@
+package daegyu;
+
+public class Daegyu0409 {
+    public static void main(String[] args) {
+
+        int num = 3;
+        double pi = 3.14;
+        String hi = "hello";
+        char small = 'A';
+        boolean check = true;
+    }
+}
